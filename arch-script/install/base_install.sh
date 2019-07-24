728479979b164854c48d074612e6f8863075c981
Lucien Martin Gaffé <lucien.martingaffe@gmail.com>
2019-07-24 22:22:22 +0200

    adding arch script maybe not for ever

---
+++ arch-script/install/base_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Minimal Installation
+# Install arch base
+# Add ntfs-3g and os-prober to detect windows in dual-boot case
+# Add bash completion
+pacstrap /mnt base base-devel grub os-prober ntfs-3g bash-completion
+
+if [ $? -eq 0 ]; then
+    echo "Arch linux base installed"
+else
+    echo "Installation failed."
+fi
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Change root into the new system:
+arch-chroot /mnt
+
+
+
+
---
+++ arch-script/install/setup_install.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Global Variable:
+KEYBOARD_AVAIL=('fr' 'en')
+KEYBOARD='en'
+
+EFI_MODE=false
+
+# Color
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
+echo "\nArch install script\n"
+
+# Load keyboard
+printf '%s\n' "${KEYBOARD_AVAIL}"
+read -p "Choose your keyboard layout: " KEYBOARD
+
+loadkeys $KEYBOARD
+
+if [ $? -eq 0 ]; then
+    echo "${GREEN}Keyboard setup.${NC}"
+else
+    echo "${RED}Keyboard setup fail.${NC}"
+fi
+
+# Check if the machine is in EFI or CSM mode
+# Folder efi exit only in mode EFI
+if [ -d "/sys/firmware/efi/efivars" ]; then
+    echo "UEFI mode is enabled."
+    EFI_MODE=true
+else
+    echo "CSM mode is enabled."
+    EFI_MODE=false
+fi
+
+# Check if network is up
+# ping -> -c nb = the number of request to send
+#      -> -q = quiet output
+#      -> -W nb = time to wait for a response 
+if ping -q -c 1 -W 3 google.com >/dev/null; then
+    echo "${GREEN}The network is up.${NC}"
+else
+    echo "${RED}The network is down.${NC}"
+    read -p "Do you want to connect to through wi-fi ? [Y/n]: " DIALOG_ANSWER
+    if [[ $DIALOG_ANSWER=~ ^[Yy]$ ]]; then
+        wifi-menu
+    else
+        echo "Impossible to do the installation without internet."
+        echo "EXIT"
+        exit
+    fi
+fi
+
+# Update the system clock 
+timedatectl set-ntp true
+
+printf "${YELLOW}Time for you to partition your disk.${NC}"
+
